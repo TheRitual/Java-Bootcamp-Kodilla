3170ba5556ffc551e0e14a51a1b5904e458e818e
TheRitual <dev@theritual.eu>
2019-07-15 22:17:45 +0200

    Created ForumPost class

--- kodilla-testing/src/main/java/com/kodilla/testing/ForumUser/ForumPost.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/ForumUser/ForumPost.java
@@ -1,4 +1,18 @@
 package com.kodilla.testing.ForumUser;
 
+import java.sql.Timestamp;
+import java.util.UUID;
+
 public class ForumPost {
+    private String authorUuid;
+    private String body;
+    private long timeStamp;
+    private String uuid;
+
+    public ForumPost (String userUuid, String postUuid, String body) {
+        this.timeStamp = new Timestamp(System.currentTimeMillis()).getTime();
+        this.authorUuid = userUuid;
+        this.body = body;
+        this.uuid = UUID.randomUUID().toString();
+    }
 }

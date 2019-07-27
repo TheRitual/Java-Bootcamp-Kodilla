a8a16178ba816e97a75ca3865199c87cc41566f5
TheRitual <dev@theritual.eu>
2019-07-27 10:31:47 +0200

    Printing "The Kill" with lambdas

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.lambda.SaySomething;
+
 public class StreamMain {
     public static void main(String[] args) {
         System.out.println("Welcome to module 7 - STREAMS!");
+
+        SaySomething ss = new SaySomething();
+        ss.say();
+
+        Processor proc = new Processor();
+        ExecuteSaySomething ess = new ExecuteSaySomething();
+        proc.execute(ess);
+
+        Executor thirdLine = () -> System.out.println("What would you do?");
+        proc.execute(thirdLine);
+
+        proc.execute(() -> System.out.println("What if I fell to the floor?"));
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/SaySomething.java
@@ -0,0 +1,7 @@
+package com.kodilla.stream.lambda;
+
+public class SaySomething {
+    public void say() {
+        System.out.println("What if I wanted to break?");
+    }
+}

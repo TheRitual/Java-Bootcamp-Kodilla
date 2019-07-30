cd873008b8b5a14f8210dedc67e4486ddbc53c1a
TheRitual <dev@theritual.eu>
2019-07-30 16:12:49 +0200

    Second Challange is throwing exception and it's good!

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandler.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandler.java
@@ -2,6 +2,12 @@ package com.kodilla.exception.test;
 
 public class ExceptionHandler {
     public static void main(String[] args) {
+        SecondChallenge sc = new SecondChallenge();
 
+        try{
+            sc.probablyIWillThrowException(10,29);
+        } catch(Exception e) {
+            System.out.println("Wrong stuff is happening! " + e);
+        }
     }
 }

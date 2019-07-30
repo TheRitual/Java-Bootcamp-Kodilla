b18aa11e5446b531c9d6e38b0c01578e13fab3aa
TheRitual <dev@theritual.eu>
2019-07-30 15:53:15 +0200

    Don't divide by 0! EVER!

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -12,7 +12,11 @@ public class FirstChallenge {
 
         FirstChallenge firstChallenge = new FirstChallenge();
         try {
-            double result = firstChallenge.divide(3, 0);
+            System.out.println("First 3/2 -> ");
+            double result = firstChallenge.divide(3, 2);
+            System.out.println(result);
+            System.out.println("Second 3/0 -> ");
+            result = firstChallenge.divide(3, 0);
             System.out.println(result);
         } catch (ArithmeticException e) {
             System.out.println("You almost destroyed a world! Don't divide by 0! " + e);

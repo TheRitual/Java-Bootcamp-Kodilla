1f8d980a010db5ba02c0dc1bc07337f7621b1116
Ritual <dev@theritual.eu>
2020-10-01 17:40:37 +0200

    Creating beans from method which make random class implementing Shape interface

--- kodilla-spring/src/main/java/com/kodilla/spring/shape/ShapesFactory.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/shape/ShapesFactory.java
@@ -3,10 +3,26 @@ package com.kodilla.spring.shape;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Random;
+
 @Configuration
 public class ShapesFactory {
     @Bean
     public Square createSquare() {
         return new Square();
+    }
+
+    @Bean
+    public Shape chosenShape() {
+        Shape theShape;
+        Random generator = new Random();
+        int chosen = new Random().nextInt(3);
+        switch(chosen){
+            case 0 : theShape = new Circle(); break;
+            case 1 : theShape = new Triangle(); break;
+            case 2 : theShape = new Square(); break;
+            default: theShape = null;
+        }
+        return theShape;
     }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -49,4 +49,15 @@ public class SpringRunnerTestSuite {
         //Then
         Assert.assertEquals("This is square", name);
     }
+
+    @Test
+    public void testShapeLoadedIntoContainer() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = (Shape) context.getBean("chosenShape");
+        //When
+        String name = shape.getShapeName();
+        //Then
+        System.out.println("Shape random introduction ::: " + name);
+    }
 }
